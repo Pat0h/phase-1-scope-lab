9ba470c263514f7d8b3e0c1c6a38a3b3d9e10514
Patrick Ngatia <patrickngatia14@gmail.com>
2023-10-03 09:45:25 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function returnCustomerName() {
+  return customerName;
+
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+const leastCustomer = 'panda';
+function changeLeastFavoriteCustomer(){
+  leastCustomer ='Anyone';
+}
